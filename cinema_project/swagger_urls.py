be8d808f3cd3f2a6d7b34568f13fe5946833a839
Maksim <makscoor9@gmail.com>
2025-12-12 21:06:23 +0300

    Fix redoc configuration: remove authentication_classes when public=True

--- cinema_project/swagger_urls.py
+++ cinema_project/swagger_urls.py
@@ -2,7 +2,6 @@ from django.urls import path, re_path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -13,7 +12,6 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
-    authentication_classes=(JWTAuthentication,),
 )
 
 
